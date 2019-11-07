356fe321fb0930a11c73d7c4bf7aae9d139abfbe
rasoolmaghareh <rasool@comp.nus.edu.sg>
2019-11-07 16:07:32 +0800

    Changing output

--- src/tool/main.cpp
+++ src/tool/main.cpp
@@ -11,6 +11,7 @@
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Support/raw_ostream.h>
 
+
 using namespace std;
 using namespace llvm;
 
@@ -25,19 +26,37 @@ int main(int argc, char **argv) {
     InputFile = "../test/test.bc";
   }
   unique_ptr<Module> mainModule = parseIRFile("../test/test.bc", Err, Context);
-  errs() << "Function & BB:\n";
+  //errs() << "Function & BB:\n";
   for (Function &f : mainModule->getFunctionList()) {
     stringstream ss;
-    ss << f.getName().str() << ":";
+    ss << "max:";
     int i = 0;
     for (BasicBlock &bb : f) {
-      ss << "BB" << ++i;
+      ss  << "*" << f.getName().str() << "_" << "BB" << ++i;
       if (&f.back() != &bb) {
         ss << "+";
       }
     }
-    errs() << ss.str() << "\n";
+    errs() << ss.str() << "+";
   }
+  errs() << ";\n";
+  
+  //....
+  
+  errs() << "\n\n";
+  for (Function &f : mainModule->getFunctionList()) {
+    stringstream ss;
+    ss << "int ";
+    int i = 0;
+    for (BasicBlock &bb : f) {
+      ss << f.getName().str() << "_" << "BB" << ++i;
+      if (&f.back() != &bb) {
+        ss << ",";
+      }
+    }
+    errs() << ss.str() << ",";
+  }
+  errs() << ";\n";
 
   return 0;
 }
